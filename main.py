6939daf0cf3e3e686260013414061e8059248fcd
Dimitrios Stergiou <dstergiou@gmail.com>
2015-07-11 15:33:34 +0200

    added comments

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ if len(sys.argv) < 2:
 # Assign the letters we got from the command line to the rack variable
 rack = sys.argv[1]
 
+# Initialize an empty wordlist
 wordlist = []
 
+# Open the Scrabble word list, exit if the file is not present
 try:
     with open("words.txt", "r") as f:
         for line in f:
@@ -18,6 +20,7 @@ except EnvironmentError:
     print ("Cannot find wordlist")
     exit(1)
 
+# Initialize an empty wordlist to hold valid words
 valid_words = []
 
 for word in wordlist:
@@ -32,8 +35,10 @@ for word in wordlist:
     if candidate == True:
         valid_words.append([len(word), word])
 
+# Sort the valid words
 valid_words.sort()
 
+# Print words in the format (number of letters, word)
 for entry in valid_words:
     length = entry[0]
     word = entry[1]
