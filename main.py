4637e1f20c2c5cab4d8b8d52e554e539118555e1
Dimitrios Stergiou <dstergiou@gmail.com>
2015-07-11 17:13:38 +0200

    added while true loop

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
 import sys
-import argparse
 
-# Use an option parser
-parser = argparse.ArgumentParser()
-parser.add_argument('letters', help='Letter to use for finding word')
-parser.add_argument('preference', nargs='?', help='Preferred letter')
-args = parser.parse_args()
-
-# Assign the letters we got from the command line to the rack variable
-rack = args.letters
-
-# Initialize lists
+# Initialize list
 wordlist = []
-valid_words = []
 
 # Open the Scrabble word list, exit if the file is not present
 try:
@@ -24,27 +13,40 @@ except EnvironmentError:
     print ("Cannot find wordlist")
     exit(1)
 
-for word in wordlist:
-    candidate = True
-    rack_letters = list(rack)
-    for letter in word:
-        if letter not in rack_letters:
-            candidate = False
-            break
-        else:
-            rack_letters.remove(letter)
-    if candidate == True:
-        valid_words.append([len(word), word])
+while True:
+    # Initialize valid word list
+    valid_words = []
 
-# Sort the valid words
-valid_words.sort()
+    # Get user input
+    query = input("> ")
+    if " " in query:
+        rack, preference = query.split()
+    else:
+        rack = query
+        preference = None
 
-# Print words in the format (number of letters, word)
-for entry in valid_words:
-    length = entry[0]
-    word = entry[1]
-    if args.preference:                     # If we had a preferred letter
-        if str(args.preference) in word:    # Check if the letter is in the word
-            print(str(length) + " " + word) # And print only words containing the preferred letter
-    else:                                   # else
-        print(str(length) + " " + word)     # print all matching words
+    # Find the words
+    for word in wordlist:
+        candidate = True
+        rack_letters = list(rack)
+        for letter in word:
+            if letter not in rack_letters:
+                candidate = False
+                break
+            else:
+                rack_letters.remove(letter)
+        if candidate == True:
+            valid_words.append([len(word), word])
+
+    # Sort the valid words
+    valid_words.sort()
+
+    # Print words in the format (number of letters, word)
+    for entry in valid_words:
+        length = entry[0]
+        word = entry[1]
+        if preference:                     # If we had a preferred letter
+            if str(preference) in word:    # Check if the letter is in the word
+                print(str(length) + " " + word) # And print only words containing the preferred letter
+        else:                                   # else
+            print(str(length) + " " + word)     # print all matching words
